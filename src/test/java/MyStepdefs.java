1bd75a6b5690d944a6386d4d48b6d418ceb10429
oleksandr-yefymov <javadev173@gmail.com>
2020-01-07 21:02:39 +0200

    Cucumber basic test scenarioos

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,32 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+class IsItFriday {
+
+  static String isItFriday(String today) {
+    return "friday".equalsIgnoreCase(today) ? "Yes" : "Nope";
+  }
+}
+
+public class MyStepdefs {
+  private String today;
+  private String actualAnswer;
+
+  @Given("Today is {string}")
+  public void today_is(String day) {
+    today = day;
+  }
+
+  @When("I ask whether it's Friday yet")
+  public void i_ask_whether_it_s_Friday_yet() {
+    actualAnswer = IsItFriday.isItFriday(today);
+  }
+
+  @Then("I should be told {string}")
+  public void i_should_be_told(String expectedAnswer) {
+    assertEquals(expectedAnswer, actualAnswer);
+  }
+}
